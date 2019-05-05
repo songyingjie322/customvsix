d12ce448a8dad16e5981d280c7202bc05f1e32dc
songyingjie322 <songyingjie168@126.com>
2019-05-05 14:27:55 +0800

    初始代码

---
+++ CustomVsix/CustomVsix/OpenSameNameJs.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.IO;
+using EnvDTE;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Task = System.Threading.Tasks.Task;
+
+namespace CustomVsix
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class OpenSameNameJs
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("3098f74b-b291-4ce8-bd62-bdcdf84cf9fe");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OpenSameNameJs"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        private OpenSameNameJs(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.ExecuteAsync, menuCommandID);
+            commandService.AddCommand(menuItem);
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static OpenSameNameJs Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            // Switch to the main thread - the call to AddCommand in OpenSameNameJs's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync((typeof(IMenuCommandService))) as OleMenuCommandService;
+            Instance = new OpenSameNameJs(package, commandService);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private async void ExecuteAsync(object sender, EventArgs e)
+        {
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+            //ThreadHelper.ThrowIfNotOnUIThread();
+            var dte = (DTE)await ServiceProvider.GetServiceAsync(typeof(DTE));
+            if (dte != null && dte.Solution.Projects.Count > 0)
+            {
+                var items = dte.SelectedItems;
+                if (items.Count > 0)
+                {
+                    var projectItem = items.Item(1).ProjectItem;
+                    if (projectItem != null && !projectItem.IsDirty && projectItem.Document != null)
+                    {
+                        var lastIndex = projectItem.Name.IndexOf('.');
+                        if (lastIndex > -1)
+                        {
+                            var path = Path.Combine(projectItem.Document.Path, projectItem.Name.Substring(0, lastIndex) + (projectItem.Name.Substring(lastIndex) == ".js" ? ".cshtml" : ".js"));
+                            if (File.Exists(path))
+                                dte.ExecuteCommand("File.OpenFile", path);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                VsShellUtilities.ShowMessageBox(
+                    this.package,
+                    "请选择cshtml/js文件",
+                    "插件提示",
+                    OLEMSGICON.OLEMSGICON_INFO,
+                    OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                    OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+            }
+        }
+    }
+}
